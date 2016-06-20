44257a330170c95e258a2fabd6b48569d1ad7e41
20032410 <252388810@qq.com>
2016-06-20 10:41:01 +0800

    add new

---
+++ ex40.py
@@ -0,0 +1,16 @@
+#  ex40.py 
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print line
+
+bulls_on_parade = Song(["They rally around the family",
+	                    "With pockets full of shells"])
+
+# happy_bday.sing_me_a_song()
+
+bulls_on_parade.sing_me_a_song()
